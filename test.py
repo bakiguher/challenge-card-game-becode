748d658b5c31ecc6e2fdd103d0828249ff7fd32f
Baki <bakiguher@hotmail.com>
2022-02-23 09:50:04 +0100

    formatted

--- test.py
+++
@@ -1,9 +0,0 @@
-while True:
-    num = input("Please enter a number ")
-    try:
-        val = int(num)
-        print("Input is an integer number.")
-        print("Input number is: ", val)
-        break
-    except ValueError:
-        print("Input is not integer.")
